8c5ffad73c19e0e37ee6ddbd9c876d94938b7935
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-06-12 16:56:00 +0200

    sth works

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -16,53 +15,39 @@ const (
 	NormalUser = iota // 1
 )
 
+// Create structs for our data models
 type User struct {
 	gorm.Model
 	Name     string
-	Surname  string
 	Email    string
 	Password string
 	Role     int
-	Groups   []Group `gorm:"many2many:GroupRefer"`
+	Groups   []*Group `gorm:"many2many:user_groups;"`
 }
 type Group struct {
 	gorm.Model
-	Name string
-	// Tasks      []Task `gorm:"many2many:task_id"`
-	// GroupRefer uint
-	// Users []User `gorm:"many2many:ID"`
+	Name  string
+	Users []*User `gorm:"many2many:user_groups;"`
+	Tasks []*Task `gorm:"many2many:group_tasks;"`
 }
 
 type Task struct {
-	ID        int64 `gorm:"column:task_id"`
-	CreatedAt time.Time
-	Name      string
-	Details   string
-	StartDate int64
-	EndDate   int64
-	// Group     Group `gorm:"many2many:ID;"`
+	gorm.Model
+	Name        string
+	Description string
+	Done        bool
+	Group       *[]Group `gorm:"many2many:group_tasks;"`
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	dsn := "postgres://postgres:postgrespw@localhost:49155"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Group{})
-	db.AutoMigrate(&Task{})
-	// // create example user
-	// user := User{
-	// 	Name:     "John",
-	// 	Surname:  "Doe",
-	// 	Email:    "tomasz@palinski.pl",
-	// 	Password: "password",
-	// 	Role:     Admin,
-	// }
-	// group := Group{
-	// 	Name: "Group 1",
-	// }
-	// db.Create(&group)
+	db.AutoMigrate(&User{}, &Group{}, &Task{})
+	// db.AutoMigrate(&Task{})
+
 	app := fiber.New(fiber.Config{
 		BodyLimit: 1024 * 1024 * 512, // 512MB
 	})
@@ -71,9 +56,11 @@ func main() {
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
 	v1.Get("/getusers", func(c *fiber.Ctx) error {
+		// Get all users and his groups
 		var users []User
-		db.Find(&users)
+		db.Preload("Groups").Find(&users)
 		return c.JSON(users)
+
 	})
 	v1.Post("/adduser", func(c *fiber.Ctx) error {
 		var user User
@@ -93,25 +80,64 @@ func main() {
 	})
 	v1.Get("/getgroups", func(c *fiber.Ctx) error {
 		var grupa []Group
-		db.Find(&grupa)
+		db.Preload("Users").Preload("Tasks").Find(&grupa)
 		return c.JSON(grupa)
 	})
-	v1.Post("/addusertogroup", func(c *fiber.Ctx) error {
+	v1.Post("/addgrouptouser", func(c *fiber.Ctx) error {
 		var data struct {
-			UserId  int64 `json:"userId"`
-			GroupId int64 `json:"groupId"`
+			UserID  uint
+			GroupID uint
 		}
 		if err := c.BodyParser(&data); err != nil {
 			return err
 		}
-		// add user to group
+		
 		var user User
-		db.First(&user, data.UserId)
+		// db.First(&user, data.UserID, 1)
+		db.Model(&User{}).Where("id = ?", data.UserID).First(&user)
 		var group Group
-		db.First(&group, data.GroupId)
-		user.Groups = append(user.Groups, group)
+		// db.First(&group, data.GroupID, 1)
+		db.Model(&Group{}).Where("id = ?", data.UserID).First(&group)
+		if group. == nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Group not found")
+		}
+
+		user.Groups = append(user.Groups, &group)
 		db.Save(&user)
 		return c.JSON(user)
+
+	})
+	v1.Post("/addtask", func(c *fiber.Ctx) error {
+		var task Task
+		if err := c.BodyParser(&task); err != nil {
+			return err
+		}
+		db.Create(&task)
+		return c.JSON(task)
+	})
+	v1.Post("/assigntask", func(c *fiber.Ctx) error {
+		var data struct {
+			TaskID  uint
+			GroupID uint
+		}
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+		var task Task
+		// db.First(&user, data.UserID, 1)
+		db.Model(&Task{}).Preload("Group").Where("id = ?", data.TaskID).First(&task)
+		var group Group
+		// db.First(&group, data.GroupID, 1)
+		db.Model(&Group{}).Where("id = ?", data.GroupID).First(&group)
+
+		group.Tasks = append(group.Tasks, &task)
+		db.Save(&task)
+		return c.JSON(task)
+	})
+	v1.Get("/gettasks", func(c *fiber.Ctx) error {
+		var task []Task
+		db.Preload("Group").Find(&task)
+		return c.JSON(task)
 	})
 	log.Fatal(app.Listen(":3000"))
 }
